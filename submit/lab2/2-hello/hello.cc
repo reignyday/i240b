92f28e46b627db623a72235c7fdb3c51b4f99008
Rein Bey <rbey1@cs.binghamton.edu>
2020-02-06 19:03:15 -0500

    completed lab2

---
+++ submit/lab2/2-hello/hello.cc
@@ -0,0 +1,51 @@
+#include <locale>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+/** Upcase the initial letter of each word from in, copying the
+ *  results into out.  The caller is responsible to ensure that out
+ *  has enough space for the strlen(in) characters of in.
+ */
+void
+upcaseWords(const char in[], char out[])
+{
+  bool inWord = false;
+  //changed n to plus one in order to include the null character at the end of
+  //a string
+  const int n = std::strlen(in)+1;
+  for (int i = 0; i < n; i++) {
+    if (!inWord && std::isalpha(in[i])) {
+      out[i] = std::toupper(in[i]);
+    }
+    else {
+      out[i] = in[i];
+    }
+    inWord = std::isalpha(in[i]);
+  }
+}
+
+static void
+sayHello(const char *name)
+{
+  std::cout << "Hello " << name << std::endl;
+}
+
+
+int
+main(int argc, const char *argv[])
+{
+  if (argc == 1) {
+    std::cerr << "usage: " << argv[0] << " NAME..." << std::endl;
+    std::exit(1);
+  }
+  else {
+    //removed i<=argc
+    for (int i = 1; i < argc; i++) {
+      const char *name = argv[i];
+      char friendlyName[strlen(name)];
+      upcaseWords(name, friendlyName);
+      sayHello(friendlyName);
+    }
+  }
+}
